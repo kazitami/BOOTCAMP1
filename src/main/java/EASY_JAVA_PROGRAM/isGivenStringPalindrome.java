8801d5f8bb7882fa3f38eca10e16106e4f66de7a
kazitami <kazitami@gmail.com>
2024-04-26 00:28:49 -0400

    String palindrome or not

--- src/main/java/EASY_JAVA_PROGRAM/isGivenStringPalindrome.java
+++ src/main/java/EASY_JAVA_PROGRAM/isGivenStringPalindrome.java
@@ -14,9 +14,11 @@ public class isGivenStringPalindrome {
 		        boolean isPalindrome = true;
 		        for (int i = 0; i < input.length() / 2; i++) {
 		            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+		            	
 		                isPalindrome = false;
 		                break;
 		            }
+		            System.out.println(i);
 		        }
 
 		        // Print the result
